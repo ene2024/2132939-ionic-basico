f1a85de0ef66f09a17af81cf94673b45eef430bf
Jose Luis Calderon Galarza <142639379+JL1704@users.noreply.github.com>
2024-02-21 23:31:49 -0600

    Practica 4

--- funcion.js
+++ funcion.js
@@ -7,7 +7,20 @@ var total = 0;
 function agregar(){
     articulo=document.getElementById("articulo").value;
     monto=document.getElementById("monto").value;
-    
+    monto = parseInt(monto);
+    if(isNaN(monto)) {
+        alert("Por favor, ingrese un monto válido.");
+        return; 
+    }
+
+    let divLista =document.getElementById("Lista");
     let elementoTicket = document.createElement("p");
-    elementoTicket.innerHTML = articulo + "...." + monto;
+    elementoTicket.textContent = articulo + "...." + monto;
+    divLista.innerHTML = elementoTicket.outerHTML;
+
+    total += monto
+    document.getElementById("total").textContent = "Total: " + total;
+
+    document.getElementById("articulo").value = "";
+    document.getElementById("monto").value = "";
 }
