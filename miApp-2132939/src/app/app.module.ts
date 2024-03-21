167cc76340ac77d659f6bb9710f89f9cf24adf34
Jose Luis CG <142639379+JL1704@users.noreply.github.com>
2024-03-20 22:21:09 -0600

    Practica 7 Final

--- miApp-2132939/src/app/app.module.ts
+++ miApp-2132939/src/app/app.module.ts
@@ -5,12 +5,17 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+//import { ListaAlumnosComponent } from '/Users/josel/OneDrive/Escritorio/AppsMov/2132939/miApp-2132939/src/app/lista-alumnos/lista-alumnos.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    //ListaAlumnosComponent // Declara el componente ListaAlumnosComponent aquí
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
--- miApp-2132939/src/app/home/home.module.ts
+++ miApp-2132939/src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { ListaAlumnosComponent } from '../lista-alumnos/lista-alumnos.component';
 
 @NgModule({
   imports: [
@@ -14,6 +14,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,
+    ListaAlumnosComponent]
 })
 export class HomePageModule {}
