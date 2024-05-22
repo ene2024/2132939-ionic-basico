a7707062ffb9dedd7000252675cdea24aafff92f
Jose Luis CG <142639379+JL1704@users.noreply.github.com>
2024-05-21 18:33:54 -0600

    Practica #14 - Splash screen

--- miApp-2132939/android/app/src/main/java/com/example/app/MainActivity.java
+++ miApp-2132939/android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,31 @@
 package com.example.app;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    TimerTask tarea = new TimerTask(){
+      @Override
+      public void run(){
+        Intent intent = new Intent(MainActivity.this, PrincipalActivity.class);
+        startActivity(intent);
+        finish();
+      }
+    };
+
+    Timer tiempo = new Timer();
+    tiempo.schedule(tarea,3000);
+  }
+}
